303ccd16ebbe6502e7549bbf5136ec9b661f5005
Philip Oyelegbin <philipoyelegbin@gmail.com>
2023-01-16 20:43:01 +0100

    feat: modals created for the about page and made responsive

---
+++ frontend/src/pages/about/components/card-modal/CardModal.jsx
@@ -0,0 +1,38 @@
+import ReactDOM from 'react-dom';
+import './card-modal.scss';
+import close from 'assets/svgs/close-icon.svg'
+
+const CardModal = (props) => {
+    const {foundersDetails, teamDetails, closeModal} = props
+
+  return ReactDOM.createPortal(
+    <section className="overlay" onClick={closeModal}>
+        {foundersDetails?.map((founder, index)=> (
+            <figure className="d-flex card" key={index} onClick={(e) => e.stopPropagation()}>
+                <img src={founder.avatar} loading="lazy" className="avatar" alt={`profile avatar of ${founder.full_name}`} />
+                <figcaption>
+                    <img src={close} loading="lazy" className="close-btn" onClick={closeModal} alt=""/>
+                    <h3 className='big-heading-3'>{founder.full_name}</h3>
+                    <h4 className='big-heading-4'>{founder.position}</h4>
+                    <p>{founder.summary}</p>
+                </figcaption>
+            </figure>
+        ))}
+
+        {teamDetails?.map((team, index)=> (
+            <figure className="d-flex card" key={index}>
+                <img src={team.avatar} loading="lazy" className="avatar" alt={`profile avatar of ${team.full_name}`} />
+                <figcaption>
+                    <p className="close-btn" onClick={closeModal}>X</p>
+                    <h3 className='big-heading-3'>{team.full_name}</h3>
+                    <h4 className='big-heading-4'>{team.position}</h4>
+                    <p>{team.summary}</p>
+                </figcaption>
+            </figure>
+        ))}
+    </section>,
+    document.getElementById('portal')
+  )
+}
+
+export default CardModal
---
+++ frontend/src/pages/about/components/team-section/data.js
@@ -0,0 +1,50 @@
+import catherine from 'assets/svgs/catherine.svg'
+import james from 'assets/svgs/james.svg'
+import graham from 'assets/svgs/graham.svg'
+import laura from 'assets/svgs/laura.svg'
+import abdul from 'assets/images/abdul.png'
+import mehran from 'assets/images/mehran.png'
+import gaynor from 'assets/svgs/gaynor.svg'
+import sam from 'assets/svgs/sam.svg'
+import hauwa from 'assets/images/hauwa.png'
+import obinna from 'assets/images/obinna.png'
+import philip from 'assets/images/philip.png'
+
+export const founders = [
+    {
+        avatar: graham, full_name: "Prof. Graham Bonwick", position: "Director / Co-Founder", summary: "Graham has over 35 years applied science expertise that encompasses food safety, environmental quality, exploitation of Agri-Food by-products and packaging technology. He was formerly a Lead Scientist at FERA Science Ltd with a joint appointment at Newcastle University Institute for Agri-Food Research & Innovation. Prior to joining FERA established the NoWFood Centre and was an Institute Director at University of Chester. He continues to contribute to a range of taught programmes at several universities in areas such as food science, nutrition and dietetics and chemical engineering.", email: "mailto: ", linkedin: "//"
+    },
+    {
+        avatar: catherine, full_name: "Dr. Catherine Birch", position: "Director / Co-Founder", summary: "Catherine has a broad range of expertise that includes support for innovation and new product development and management of projects and industrial networks to aid growth of biotechnology and food SMEs. She was formerly a Senior Research Fellow at Newcastle University and a project manager at FERA Science Ltd. Her research expertise includes functional foods and packaging technology. She is a Series Editor for the Royal Society Chemistry book series Food Chemistry, Function & Analysis and Green Chemistry and is a referee for the International Journal of Food Science & Technology.", email: "mailto: ", linkedin: "//"
+    },
+    {
+        avatar: james, full_name: "James Bonwick", position: "Technical Advisor", summary: "James is an Engineering Manager at Thirdfort Limited. He was formerly the Head of Technology at Wombat Invest where he continues to act as a technology advisor. His experience includes development work at Netsells Ltd and Morgan Stanley.", email: "mailto: ", linkedin: "//"
+    },
+    {
+        avatar: laura, full_name: "Laura Love", position: "Business Advisor", summary: "Laura has over 14 years’ project management experience within financial services, insurance, and retail banking; predominantly in mergers and acquisitions within large blue-chip companies. She is the director and business investor for 3L Investment Ltd and works closely with a dynamic community of small businesses, private investors, wealth managers, HR, Media and Technology specialists across a multitude of sectors, along with highly experienced accountants and solicitors.", email: "mailto: ", linkedin: "//"
+    }
+]
+
+export const devTeams = [
+    {
+        avatar: abdul, full_name: "Abdulsamad Gobir", position: "Product Manager", summary: "", email: "mailto: ", linkedin: "//"
+    },
+    {
+        avatar: mehran, full_name: "Mehran Fani", position: "Product Owner", summary: "Mehran Fani comes from a technology and engineering background. He has degrees in both mechanical and automation engineering and experience with business development, product management, and sustainability.", email: "mailto: ", linkedin: "//"
+    },
+    {
+        avatar: gaynor, full_name: "Gaynor Bonwick", position: "Research Assistant", summary: "Gaynor is a biologist and analytical biochemist by training and has worked on several projects for the World Health Organization. She has worked in environmental testing labs and in the waste management sector.  Passionate about sustainability, she is working to drive progress towards a circular economy.", email: "mailto: ", linkedin: "//"
+    },
+    {
+        avatar: sam, full_name: "Sam Davidowitz-Neu", position: "Product Designer", summary: "", email: "mailto: ", linkedin: "//"
+    },
+    {
+        avatar: hauwa, full_name: "Hauwa Opaluwa", position: "Frontend Engineer", summary: "Hauwa Opaluwa is a result-driven Front-end developer with experience building projects with technologies such as ReactJS, HTML, CSS, JavaScript, and Bootstrap.", email: "mailto: ", linkedin: "//"
+    },
+    {
+        avatar: philip, full_name: "Philip Oyelegbin", position: "Frontend Engineer", summary: "Philip is a React Developer and a Robotics Instructor based in Lagos, Nigeria. He graduated from Yaba College of Technology with a degree in Industrial Maintenance Engineering. He is passionate about creating digital solutions.", email: "mailto: philipoyelegbin@gmail.com", linkedin: "//linkedin.com/in/philipoyelegbin"
+    },
+    {
+        avatar: obinna, full_name: "Obinna Uko Ofe", position: "Software Engineer", summary: "", email: "mailto: ", linkedin: "//linkedin.com/in/obinna-ofe-8b07a8124"
+    }
+]
