8b967523e52a59367a50cadb7f42739c3ed5c35f
Philip Oyelegbin <philipoyelegbin@gmail.com>
2023-01-22 19:10:43 +0100

    fix: the modal hover state has been added.

--- frontend/src/pages/about/components/card-modal/CardModal.jsx
+++ frontend/src/pages/about/components/card-modal/CardModal.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
-import './card-modal.scss';
 import close from 'assets/svgs/close-icon.svg'
+import './card-modal.scss';
 
 const CardModal = (props) => {
     const {foundersDetails, teamDetails, closeModal} = props
@@ -9,9 +9,11 @@ const CardModal = (props) => {
     <section className="overlay" onClick={closeModal}>
         {foundersDetails?.map((founder, index)=> (
             <figure className="d-flex card" key={index} onClick={(e) => e.stopPropagation()}>
-                <img src={founder.avatar} loading="lazy" className="avatar" alt={`profile avatar of ${founder.full_name}`} />
+                <div className="avatar-container">
+                    <img src={founder.avatar} loading="lazy" className="avatar" alt={`profile avatar of ${founder.full_name}`} />
+                </div>
                 <figcaption>
-                    <img src={close} loading="lazy" className="close-btn" onClick={closeModal} alt=""/>
+                    <img src={close} loading="lazy" className="close-btn" onClick={closeModal} alt="close icon"/>
                     <h3 className='big-heading-3'>{founder.full_name}</h3>
                     <h4 className='big-heading-4'>{founder.position}</h4>
                     <p>{founder.summary}</p>
--- frontend/src/pages/about/components/team-section/data.js
+++ frontend/src/pages/about/components/team-section/data.js
@@ -12,39 +12,39 @@ import philip from 'assets/images/philip.png'
 
 export const founders = [
     {
-        avatar: graham, full_name: "Prof. Graham Bonwick", position: "Director / Co-Founder", summary: "Graham has over 35 years applied science expertise that encompasses food safety, environmental quality, exploitation of Agri-Food by-products and packaging technology. He was formerly a Lead Scientist at FERA Science Ltd with a joint appointment at Newcastle University Institute for Agri-Food Research & Innovation. Prior to joining FERA established the NoWFood Centre and was an Institute Director at University of Chester. He continues to contribute to a range of taught programmes at several universities in areas such as food science, nutrition and dietetics and chemical engineering.", email: "mailto: ", linkedin: "//"
+        avatar: graham, full_name: "Prof. Graham Bonwick", position: "Director / Co-Founder", summary: "Graham has over 35 years applied science expertise that encompasses food safety, environmental quality, exploitation of agri-food by-products and packaging technology. He was formerly a Lead Scientist at FERA Science Ltd with a joint appointment at Newcastle University Institute for Agri-Food Research & Innovation.", email: "mailto: graham.bonwick@valorise.co.uk", linkedin: "//linkedin.com/in/grahambonwick"
     },
     {
-        avatar: catherine, full_name: "Dr. Catherine Birch", position: "Director / Co-Founder", summary: "Catherine has a broad range of expertise that includes support for innovation and new product development and management of projects and industrial networks to aid growth of biotechnology and food SMEs. She was formerly a Senior Research Fellow at Newcastle University and a project manager at FERA Science Ltd. Her research expertise includes functional foods and packaging technology. She is a Series Editor for the Royal Society Chemistry book series Food Chemistry, Function & Analysis and Green Chemistry and is a referee for the International Journal of Food Science & Technology.", email: "mailto: ", linkedin: "//"
+        avatar: catherine, full_name: "Dr. Catherine Birch", position: "Director / Co-Founder", summary: "Catherine has a broad range of expertise that includes support for innovation and new product development and management of projects and industrial networks to aid growth of biotechnology and food SMEs. She was formerly a Senior Research Fellow at Newcastle University and a project manager at FERA Science Ltd.", email: "mailto: cath.birch@valorise.co.uk", linkedin: "//linkedin.com/in/dr-catherine-birch"
     },
     {
-        avatar: james, full_name: "James Bonwick", position: "Technical Advisor", summary: "James is an Engineering Manager at Thirdfort Limited. He was formerly the Head of Technology at Wombat Invest where he continues to act as a technology advisor. His experience includes development work at Netsells Ltd and Morgan Stanley.", email: "mailto: ", linkedin: "//"
+        avatar: james, full_name: "James Bonwick", position: "Technical Advisor", summary: "James is an Engineering Manager at Thirdfort Limited. He was formerly the Head of Technology at Wombat Invest where he continues to act as a technology advisor. His experience includes development work at Netsells Ltd and Morgan Stanley.", email: "mailto: james.bonwick1994@gmail.com", linkedin: "//linkedin.com/in/james-bonwick-19a9691b1"
     },
     {
-        avatar: laura, full_name: "Laura Love", position: "Business Advisor", summary: "Laura has over 14 years’ project management experience within financial services, insurance, and retail banking; predominantly in mergers and acquisitions within large blue-chip companies. She is the director and business investor for 3L Investment Ltd and works closely with a dynamic community of small businesses, private investors, wealth managers, HR, Media and Technology specialists across a multitude of sectors, along with highly experienced accountants and solicitors.", email: "mailto: ", linkedin: "//"
+        avatar: laura, full_name: "Laura Love", position: "Business Advisor", summary: "Laura has over 14 years’ project management experience within financial services, insurance, and retail banking; predominantly in mergers and acquisitions within large blue-chip companies. She is the director and business investor for 3L Investment Ltd and works closely with a dynamic community of small businesses, private investors, wealth managers, HR, Media and Technology specialists across a multitude of sectors, along with highly experienced accountants and solicitors.", email: "mailto: lauralove@3Linvestment.co.uk", linkedin: "//linkedin.com/in/laura-love-56b35a14"
     }
 ]
 
 export const devTeams = [
     {
-        avatar: abdul, full_name: "Abdulsamad Gobir", position: "Product Manager", summary: "", email: "mailto: ", linkedin: "//"
+        avatar: abdul, full_name: "Abdulsamad Gobir", position: "Product Manager", summary: "", email: "mailto: ", linkedin: "//linkedin.com/in/"
     },
     {
-        avatar: mehran, full_name: "Mehran Fani", position: "Product Owner", summary: "Mehran Fani comes from a technology and engineering background. He has degrees in both mechanical and automation engineering and experience with business development, product management, and sustainability.", email: "mailto: ", linkedin: "//"
+        avatar: mehran, full_name: "Mehran Fani", position: "Product Owner", summary: "Mehran comes from a tech and engineering background and has several years of experience in business development, product management and consultancies in fields related to sustainability, renewable energy and green solutions. Mehran has helped to develop products where he focused on B2B agile development and lean manufacturing. He was CEO of SamaPower company and developed renewable businesses from scratch and has had a long-time cooperation with AzmaTech LLC in various projects in power generation and project developments.", email: "mailto: ", linkedin: "//linkedin.com/in/"
     },
     {
-        avatar: gaynor, full_name: "Gaynor Bonwick", position: "Research Assistant", summary: "Gaynor is a biologist and analytical biochemist by training and has worked on several projects for the World Health Organization. She has worked in environmental testing labs and in the waste management sector.  Passionate about sustainability, she is working to drive progress towards a circular economy.", email: "mailto: ", linkedin: "//"
+        avatar: gaynor, full_name: "Gaynor Bonwick", position: "Research Assistant", summary: "Gaynor is a biologist and analytical biochemist by training and has worked on several projects for the World Health Organisation. She has worked in environmental testing labs and in the waste management sector. Passionate about sustainability, she is working to drive progress towards a circular economy.", email: "mailto: gaynor.bonwick@valorise.co.uk", linkedin: "//linkedin.com/in/"
     },
     {
-        avatar: sam, full_name: "Sam Davidowitz-Neu", position: "Product Designer", summary: "", email: "mailto: ", linkedin: "//"
+        avatar: sam, full_name: "Sam Davidowitz-Neu", position: "Product Designer", summary: "", email: "mailto: sdavidowitzneu@gmail.com", linkedin: "//linkedin.com/in/samdn"
     },
     {
-        avatar: hauwa, full_name: "Hauwa Opaluwa", position: "Frontend Engineer", summary: "Hauwa Opaluwa is a result-driven Front-end developer with experience building projects with technologies such as ReactJS, HTML, CSS, JavaScript, and Bootstrap.", email: "mailto: ", linkedin: "//"
+        avatar: hauwa, full_name: "Hauwa Opaluwa", position: "Frontend Engineer", summary: "Hauwa Opaluwa is a result-driven Front-end developer with experience building projects with technologies such as ReactJS, HTML, CSS, JavaScript, and Bootstrap.", email: "mailto: hauwauopaluwa@gmail.com", linkedin: "//linkedin.com/in/hauwa-opaluwa"
     },
     {
-        avatar: philip, full_name: "Philip Oyelegbin", position: "Frontend Engineer", summary: "Philip is a React Developer and a Robotics Instructor based in Lagos, Nigeria. He graduated from Yaba College of Technology with a degree in Industrial Maintenance Engineering. He is passionate about creating digital solutions.", email: "mailto: philipoyelegbin@gmail.com", linkedin: "//linkedin.com/in/philipoyelegbin"
+        avatar: philip, full_name: "Philip Oyelegbin", position: "Frontend Engineer", summary: "Philip is a Frontend Engineer and a Robotics Instructor who currently works as a Customer Support Officer at WhoGoHost based in Lagos, Nigeria. He graduated from Yaba College of Technology with a degree in Industrial Maintenance Engineering. He is passionate about creating digital solutions for SMEs.", email: "mailto: philipoyelegbin@gmail.com", linkedin: "//linkedin.com/in/philipoyelegbin"
     },
     {
-        avatar: obinna, full_name: "Obinna Uko Ofe", position: "Software Engineer", summary: "", email: "mailto: obinnaukoofe@gmail.com", linkedin: "//linkedin.com/in/obinna-ofe-8b07a8124"
+        avatar: obinna, full_name: "Obinna Uko Ofe", position: "Software Engineer", summary: "Obinna is a Software Development Engineer and focuses on JavaScript and TypeScript Programming as a tool to build both private and public web services and web APIs for front-end and back-end applications. He currently works as a Senior Front-end Engineer at Cyberme Studio and Trusted Partners in addition to his role as a volunteer Fullstack Engineer at Valorise.", email: "mailto: obinnaukoofe@gmail.com", linkedin: "//linkedin.com/in/obinna-ofe-8b07a8124"
     }
 ]
