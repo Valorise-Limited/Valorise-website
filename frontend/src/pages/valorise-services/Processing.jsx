6fc8f6813c07ff61506952b4eecdc68ea57c0f36
Samuel Okpe <38431147+samokpe2@users.noreply.github.com>
2023-04-13 11:01:37 +0100

    Add Services Content

--- frontend/src/pages/valorise-services/Processing.jsx
+++ frontend/src/pages/valorise-services/Processing.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ClimateBanner from 'components/climate-banner/ClimateBanner';
-import ComingSoon from 'components/coming-soon/ComingSoon';
+import { Link } from 'react-router-dom'
 
 const Processing = () => {
   return (
     <div>
       <ClimateBanner bannerHeadingText="Processing Services" />
 
-      <ComingSoon />
+      <br/>
+        <article className='brand-container margin-top-64 margin-bottom-64'>
+
+        <p>We can offer access to a range of processing services for your waste. 
+Examples include: drying, milling and extraction.
+        </p>
+        <p>
+        To discuss your specific requirements please <Link className="link" to="/contact">Contact Us.</Link>
+        </p>
+        </article>
+
+        <br/>
     </div>
   )
 }
--- frontend/src/pages/valorise-services/TransportLogistics.jsx
+++ frontend/src/pages/valorise-services/TransportLogistics.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ClimateBanner from 'components/climate-banner/ClimateBanner';
-import ComingSoon from 'components/coming-soon/ComingSoon';
+
+import { Link } from 'react-router-dom'
 
 const TransportLogistics = () => {
   return (
     <div>
       <ClimateBanner bannerHeadingText="Transport | Logistics" />
 
-      <ComingSoon />
+      <article className='brand-container margin-top-64 margin-bottom-64'>
+
+        <p>We can arrange transportation for wet/dry and palletised materials
+          </p>
+        <p>
+        To discuss your specific requirements please <Link className="link" to="/contact">Contact Us.</Link>
+        </p>
+      </article>
     </div>
   )
 }
